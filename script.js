401c747c5400125a022d55b7940cbe4c527df819
accmy <iptvacc8@gmail.com>
2025-04-05 11:37:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     
-        fetch('scroll1.json')
+        fetch('https://api.jsonstorage.net/v1/json/4e66c686-c852-4cf9-a34d-5d7c2476219b/6b3875d0-9bad-4350-97b8-a54f84bab547')
             .then(response => response.text())
             .then(data => {
                 document.getElementById('marquee').innerHTML = data;
